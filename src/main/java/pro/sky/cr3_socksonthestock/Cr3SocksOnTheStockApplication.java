e455426090a2a05af248a15335b24145bc874ead
Владимир <duvla@mail.ru>
2023-03-10 22:50:10 +1000

    First commit

---
+++ src/main/java/pro/sky/cr3_socksonthestock/Cr3SocksOnTheStockApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cr3_socksonthestock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cr3SocksOnTheStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cr3SocksOnTheStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/cr3_socksonthestock/Cr3SocksOnTheStockApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cr3_socksonthestock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cr3SocksOnTheStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
